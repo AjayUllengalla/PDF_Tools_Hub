cd5e52a99c7a3b61b895a2a78f95351d8933e0d5
Ajay Ullengalla <ullengallaajay28@gmail.com>
2026-03-26 17:55:46 +0530

    Changes made

--- frontend/pdfFrontend/src/services/api.js
+++ frontend/pdfFrontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/pdf/convert/excel-to-pdf";
+const API = "http://localhost:8080/pdf/convert";
 
 export const uploadFiles = async (endpoint, files, setProgress) => {
   const formData = new FormData();
 
-  files.forEach(file => formData.append("files", file));
+  files.forEach(file => formData.append("file", file));
 
   const res = await axios.post(`${API}/${endpoint}`, formData, {
     onUploadProgress: (data) => {
